dc629d55f0b4ecb7a0bad227f3e667e34e5c65b2
mirzaei.dev <mirzaei.devv@gmail.com>
2021-10-20 11:38:21 +0330

    feat: add handleChange for getting the product value
    onChange prop is passed to inputForm component for getting the product value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import InputForm from "./components/inputForm";
 import "./App.css";
 
 function App() {
+  const [productValue, setProductValue] = useState("");
+
+  const handleChange = (productValue) => {
+    setProductValue(productValue);
+  };
+
   return (
     <div className="app">
-      <InputForm />
+      <InputForm onChange={handleChange} />
     </div>
   );
 }
