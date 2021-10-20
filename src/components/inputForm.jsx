720ffb5353fdbe5011306488e4e0b60b9d6ba03c
mirzaei.dev <mirzaei.devv@gmail.com>
2021-10-20 11:41:20 +0330

    feat: add handleChange function for setting the product value

--- src/components/inputForm.jsx
+++ src/components/inputForm.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./inputForm.module.css";
 
 const InputForm = () => {
+  const [productValue, setProductValue] = useState("");
+  const handleChange = (e) => {
+    setProductValue(e.currentTarget.value.toLowerCase());
+  };
+
   return (
     <div className={styles.container}>
       <form>
@@ -11,6 +16,8 @@ const InputForm = () => {
             className="form-control"
             id="search"
             placeholder="Search for product"
+            onChange={handleChange}
+            value={productValue}
           />
         </div>
       </form>
