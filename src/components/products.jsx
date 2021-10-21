f060acc042466bce75e80fb3c67270b17de97fb1
mirzaei.dev <mirzaei.devv@gmail.com>
2021-10-21 14:00:02 +0330

    style: build products component
    This component shows products at this point

---
+++ src/components/products.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./products.module.css";
+const Products = ({ products }) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.data} ${styles.columns}`}>
+          <h5>id</h5>
+          <h5>name</h5>
+          <h5>price</h5>
+        </div>
+        {products.map((product) => (
+          <div className={styles.data} key={product.id}>
+            <p>{product.id}</p>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Products;
