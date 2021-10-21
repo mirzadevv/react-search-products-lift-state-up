45e4266d9e8a98f32f2b21a36b6d7224196ec5f7
mirzaei.dev <mirzaei.devv@gmail.com>
2021-10-21 13:43:50 +0330

    return products data into a seperate file

---
+++ src/data/products.js
@@ -0,0 +1,14 @@
+const products = [
+  { id: "1", name: "Burger", price: 100000 },
+  { id: "2", name: "Cheeseburger", price: 200000 },
+  { id: "3", name: "Chicken", price: 320000 },
+  { id: "4", name: "RoastBeef", price: 450000 },
+  { id: "5", name: "MacAndCheese", price: 120000 },
+  { id: "6", name: "Taco", price: 520000 },
+  { id: "7", name: "ButterBurger", price: 670000 },
+  { id: "8", name: "PepperoniPizza", price: 400000 },
+];
+
+export function getProducts() {
+  return products;
+}
