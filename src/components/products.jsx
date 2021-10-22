37eacdd927a385961e53cb2db607a56d48a2cfc1
mirzaei.dev <mirzaei.devv@gmail.com>
2021-10-22 13:47:41 +0330

    fix: import useRef

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./products.module.css";
 const Products = ({ products, productValue }) => {
   const { current: cloneProducts } = useRef(products);
